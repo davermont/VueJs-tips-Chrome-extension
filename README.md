f9df1689bdf35c7426a569dff99a38a084f4c012
Dave Winzler <davermont@users.noreply.github.com>
2018-02-24 08:40:26 -0500

    move code into the vue app; replace structs object with the tips data; update vue to version 2; sandbox the extension to comply with Chrome's CSP

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # VueJs-tips-Chrome-extension
-a tip generator for VueJS Chrome extendsion 
+a tip generator for VueJS Chrome extension
+
+## Update Feb 2018:
+This app helpful when I first started using VueJS, it was a cool example of a Chrome extension that used Vue. But it always bothered me that the app was really just a wrapper around the class-like structs object, so today I decided to make it more Vue-like, and update it to use Vue2.
+
+But using Vue2 caused it to violate Chrome's content security policy, since there was no longer a CSP compliant version like there was with Vue1. And the easiest way to keep it simple and not require webpack was to sandbox the app, which required a small addition to the manifest file.
--- assest/app.js
+++ assest/app.js
@@ -1,14 +1,31 @@
-
 new Vue({
-    el:"#app",
-    data:structs.data,
-    methods:{
-          handleTipBtn:function(){
-                console.log('btn click')
-                structs.btnClick()
-                
-            }
-        
-    }   
-    
-})
+    el: "#app",
+
+	data: {
+		title: 'VueJS Tips',
+		twitter: "@larry_sigo",
+		tipsLeft: 5,
+		theTip: '',
+		theCode: ''
+	},
+
+    methods: {
+        getRandomTip: function() {
+            var tipsCount = tipsData.length
+             var rand = Math.floor((Math.random()*(tipsCount-1))+ 1)
+            return tipsData[rand]
+        },
+        showNextTip:function() {
+			if (this.tipsLeft > 0) {
+				var tip = this.getRandomTip()
+				this.theTip = tip.tip
+				this.theCode = tip.code
+				--this.tipsLeft
+			}
+        }
+    },
+
+	mounted: function() {
+        this.showNextTip()
+    }
+});
